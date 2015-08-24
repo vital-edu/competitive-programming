63e3c444f55c9342e42b4598efc23b07b70ffb6f
Eduardo Vital <vitaldu@gmail.com>
2015-08-24 00:09:12 -0300

    Solved problem 1845

--- 1845.cpp
+++ 1845.cpp
@@ -1,28 +1,37 @@
 #include <iostream>
+#include <cctype>
 
 using namespace std;
 
 int main() {
-  char ban[16] = {'s', 'j', 'b', 'z', 'p', 'z', 'v', 'x', 'S', 'J', 'B', 'Z', 'P', 'Z', 'V', 'X'};
+  char ban[14] = {'b', 'j', 'p', 's', 'v', 'x', 'z', 'B', 'J', 'P', 'S', 'V', 'X', 'Z'};
 
   char buffer[2];
-  int a = 0;
+  string r;
 
   while (cin >> noskipws >> buffer[0]) {
 
-    for (int i = 0; i < 16; i++) {
+    for (int i = 0; i < 14; i++) {
       if (buffer[0] == ban[i]){
-        if (i < 8) buffer[0] = 'f';
+        if (i < 7) buffer[0] = 'f';
         else buffer[0] = 'F';
+        break;
       }
     }
 
-    if (!(((buffer[0] == 'f') && (buffer[1] == 'f')) || ((buffer[0] == 'F') && (buffer[1] == 'F')))) {
-      cout << buffer[0];
+    if (!(((buffer[0] == 'f') && (tolower(buffer[1]) == 'f')) || ((buffer[0] == 'F') && (toupper(buffer[1]) == 'F')))) {
+      r = r + buffer[0];
     }
 
     buffer[1] =  buffer[0];
+
+    if (r.length() > 100){
+      cout << r;
+      r.clear();
+    }
   }
+
+  cout << r;
 
   return 0;
 }
