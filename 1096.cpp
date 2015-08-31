ff62c5b0dd847a2a0f34758f5f103fe5ddbabc34
Eduardo Vital <vitaldu@gmail.com>
2015-08-30 23:16:33 -0300

    Problem 1096 solved

---
+++ 1096.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int cont = 0;
+  int i = 1;
+  int j = 7;
+
+  for (int i = 1; i < 10; j--) {
+    cout << "I=" << i << " J=" << j << endl;
+    cont++;
+    if (cont == 3) {
+      i += 2;
+      j = 8;
+      cont = 0;
+    }
+  }
+
+  return 0;
+}
