54db1cd235e78664ac5dde248e268b5e839a37d2
Eduardo Vital <vitaldu@gmail.com>
2015-09-03 21:01:12 -0300

    Problem 1763 solved

---
+++ 1763.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <map>
+#include <string>
+
+using namespace std;
+
+int main() {
+  map<string, string> m;
+
+  m["brasil"] = "Feliz Natal!";
+  m["alemanha"] = "Frohliche Weihnachten!";
+  m["austria"] = "Frohe Weihnacht!";
+  m["coreia"] = "Chuk Sung Tan!";
+  m["espanha"] = "Feliz Navidad!";
+  m["grecia"] = "Kala Christougena!";
+  m["estados-unidos"] = "Merry Christmas!";
+  m["inglaterra"] = "Merry Christmas!";
+  m["australia"] = "Merry Christmas!";
+  m["portugal"] = "Feliz Natal!";
+  m["suecia"] = "God Jul!";
+  m["turquia"] = "Mutlu Noeller";
+  m["argentina"] = "Feliz Navidad!";
+  m["chile"] = "Feliz Navidad!";
+  m["mexico"] = "Feliz Navidad!";
+  m["antardida"] = "Merry Christmas!";
+  m["canada"] = "Merry Christmas!";
+  m["irlanda"] = "Nollaig Shona Dhuit!";
+  m["belgica"] = "Zalig Kerstfeest!";
+  m["italia"] = "Buon Natale!";
+  m["libia"] = "Buon Natale!";
+  m["siria"] = "Milad Mubarak!";
+  m["marrocos"] = "Milad Mubarak!";
+  m["japao"] = "Merii Kurisumasu!";
+
+  string a;
+
+  while(cin >> a) {
+    auto r = m.find(a);
+
+    if (r != m.end()) cout << m.find(a)->second << endl;
+    else cout << "--- NOT FOUND ---" << endl;
+  }
+  return 0;
+}
