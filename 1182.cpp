683e9fb7ef23675831879ad92f3ccc2bc3da7f3f
Eduardo Vital <vitaldu@gmail.com>
2015-09-25 23:17:25 -0300

    Problems solved

---
+++ 1182.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+  int l;
+  char op;
+  int qtd = 0;
+
+  cin >> l;
+  cin >> op;
+
+  double a;
+  double r = 0;
+
+  for (int i = 0; i <= 11; i++) {
+    for (int j = 0; j <= 11; j++) {
+      cin >> a;
+      if (j == l) {
+        r += a;
+        if (op == 'M') {
+          qtd++;
+        }
+      }
+    }
+  }
+
+  if (op == 'M') cout << fixed << setprecision(1) << r / qtd << endl;
+  else cout << fixed << setprecision(1) << r << endl;
+}
---
+++ 1188.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main() {
+  char op;
+  int qtd = 0;
+
+  cin >> op;
+
+  double a;
+  double r = 0;
+
+  for (int i = 0; i <= 11; i++) {
+    for (int j = 0; j <= 11; j++) {
+      cin >> a;
+      if (i + j >= 12 && i > j ) {
+        r += a;
+        if (op == 'M') {
+          qtd++;
+        }
+      }
+    }
+  }
+
+  if (op == 'M') cout << fixed << setprecision(1) << r / qtd << endl;
+  else cout << fixed << setprecision(1) << r << endl;
+}
